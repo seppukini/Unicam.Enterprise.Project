bb3af64dbbcdac74972e6d9b47762ea5fe162b69
Flaminaire000 <fereckavasil2002@gmail.om>
2024-04-15 16:14:18 +0200

    Implemented HistoryController on HistoryService and build up HistoryDto @Deputation :Sob:

---
+++ Unicam.Enterprise.Project.Web/Controllers/HistoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unicam.Enterprise.Project.Application.Models.Requests;
+using Unicam.Enterprise.Project.Application.Services;
+using Unicam.Enterprise.Project.Application.Services.Abstractions;
+using Unicam.Enterprise.Project.Application.Models.DTOs;
+
+
+namespace Unicam.Enterprise.Project.Controllers
+{
+    public class HistoryController : ControllerBase
+    {
+
+        private readonly HistoryService _historyService;
+        public HistoryController(HistoryService elem)
+        {
+            _historyService = elem;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetHistory(HistoryDto dto)
+        {
+            var elem = _historyService.GetOrderHistory(dto.StartDate, dto.EndDate, dto.UserId, dto.userRoleId, dto.pageIndex, dto.pageSize);
+            return Ok(elem);
+        }
+    }
+}
