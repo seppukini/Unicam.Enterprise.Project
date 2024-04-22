721b7e7f7184dab8cce19597bec540f376e7c081
Flaminaire000 <fereckavasil2002@gmail.om>
2024-04-22 09:56:43 +0200

    Created GetOrderHistoryRequestValidator(), Created CreateOrderRequestValidator(), Implemented GetOrderHistoryRequestValidator() @matteo1610 pls Check if EndDate GreaterThan works

---
+++ Unicam.Enterprise.Project.Application/Validators/CreateOrderRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unicam.Enterprise.Project.Application.Models.Requests;
+
+namespace Unicam.Enterprise.Project.Application.Validators
+{
+    internal class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
+    {
+    }
+}
---
+++ Unicam.Enterprise.Project.Application/Validators/GetOrderHistoryRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unicam.Enterprise.Project.Application.Models.Requests;
+
+namespace Unicam.Enterprise.Project.Application.Validators
+{
+    public class GetOrderHistoryRequestValidator : AbstractValidator<GetOrderHistoryRequest>
+    {
+        public GetOrderHistoryRequestValidator()
+        {
+
+            RuleFor(e => e.StartDate)
+                .NotEmpty().WithMessage(" Start Date must be Specified ");
+
+            RuleFor (e => e.EndDate)
+                .NotEmpty().WithMessage("Not a Date Found")
+                .GreaterThan(elem => elem.StartDate).WithMessage("Start Date could not be forward then End Date");
+
+            RuleFor(e => e.PageIndex)
+                .NotEmpty().WithMessage(" Index Page could not be an Empty Page ")
+                .GreaterThanOrEqualTo(1).WithMessage(" At least one Index Page ");
+
+            RuleFor (e => e.PageSize)
+                .Empty ().WithMessage(" Must have some size ")
+                .GreaterThanOrEqualTo(1).WithMessage(" At least size one ");
+
+        }
+    }
+}
