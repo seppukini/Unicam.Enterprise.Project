242f26e4ae4fd0052393a285629b371796df5a64
Flaminaire000 <fereckavasil2002@gmail.com>
2024-05-21 09:28:56 +0200

    Add Commemnt on Entities where neccessary

--- Unicam.Enterprise.Project.Model/Entities/Order.cs
+++ Unicam.Enterprise.Project.Model/Entities/Order.cs
@@ -6,8 +6,12 @@ public class Order
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public Address DeliveryAddress { get; set; } = new ();
+
+    // An order is compose by some courses
     public List<Course> Courses { get; set; } = new ();
+
+    // Who made the order
     public int UserId { get; set; }
-    
+
     public User? User { get; set; }
 }
--- Unicam.Enterprise.Project.Model/Entities/User.cs
+++ Unicam.Enterprise.Project.Model/Entities/User.cs
@@ -1,6 +1,7 @@
 
 namespace Unicam.Enterprise.Project.Model.Entities;
 
+// Customer class
 public class User
 {
     public int Id { get; init; }
@@ -9,6 +10,7 @@ public class User
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public Role Role { get; set; }
-    
+
+    // Dishes taken for each customer
     public IEnumerable<Order> Orders { get; set; } = new List<Order>();
 }
